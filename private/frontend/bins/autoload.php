e3260cd8d24c1d8b45dee215be96ee7f0a1176df
Stefan Fuchs <stefan@fuchs.pub>
2020-09-03 09:53:34 +0200

    Move some bins

--- private/frontend/bins/autoload.php
+++ private/frontend/bins/autoload.php
@@ -4,11 +4,11 @@ if (!defined('CORE2'))
   exit;
 
 // Frontend binaries
-require_once __DIR__.'/user/user.php';
-require_once __DIR__.'/recipes/recipe.php';
-require_once __DIR__.'/books/books.php';
 require_once __DIR__.'/home.php';
-require_once __DIR__.'/user/search.php';
+require_once __DIR__.'/books.php';
+require_once __DIR__.'/recipe.php';
+require_once __DIR__.'/search.php';
+require_once __DIR__.'/user.php';
 
 if ($Controller->isAuthenticated() && $Controller->User()->isAdmin())
   require_once __DIR__.'/admin/autoload.php';
