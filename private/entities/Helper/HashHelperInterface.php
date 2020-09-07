ef15fad0582dec04610ff870a45b1d81b2d54bcf
Stefan Fuchs <stefan@fuchs.pub>
2020-09-07 10:14:31 +0200

    Updated HashHelperTest

--- private/entities/Helper/HashHelperInterface.php
+++ private/entities/Helper/HashHelperInterface.php
@@ -5,9 +5,10 @@ namespace Surcouf\Cookbook\Helper;
 if (!defined('CORE2'))
   exit;
 
-interface IHashHelper {
+interface HashHelperInterface {
 
   public static function generate_token(int $length = 32) : ?string;
+  public static function getChecksumAlgo() : string;
   public static function getHashAlgo() : string;
   public static function hash(string $input, ?string $algorithm = null) : string;
 
--- tests/HashHelperTest.php
+++ tests/HashHelperTest.php
@@ -1,13 +1,13 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\MockObject\MockObject;
-use Surcouf\Cookbook\Controller;
+use Surcouf\Cookbook\ConfigInterface;
+use Surcouf\Cookbook\ControllerInterface;
 use Surcouf\Cookbook\Helper\HashHelper;
 
+require_once realpath(__DIR__.'/../private/entities/ConfigInterface.php');
 require_once realpath(__DIR__.'/../private/entities/ControllerInterface.php');
-require_once realpath(__DIR__.'/../private/entities/Controller.php');
-require_once realpath(__DIR__.'/../private/entities/Helper/IHashHelper.php');
+require_once realpath(__DIR__.'/../private/entities/Helper/HashHelperInterface.php');
 require_once realpath(__DIR__.'/../private/entities/Helper/HashHelper.php');
 
 /**
@@ -16,31 +16,42 @@ require_once realpath(__DIR__.'/../private/entities/Helper/HashHelper.php');
 class HashHelperTest extends TestCase
 {
 
-  /** @var Controller|MockObject */
-  private $Controller;
-
   protected function setUp() : void {
     parent::setUp();
-    $this->Controller = $this->getMockBuilder(Controller::class)
-      ->disableOriginalConstructor()
-      ->disableOriginalConstructor()
-      ->getMock();
   }
 
   /**
    * @covers HashHelper::generate_token
+   * @dataProvider tokenDataProvider
    */
-  public function testGenerate_token() {
-    $this->assertIsString(HashHelper::generate_token());
+  public function testGenerate_token(int $length, int $expected) {
+    $result = HashHelper::generate_token($length);
+    $this->assertIsString($result);
+    $this->assertEquals($expected, strlen($result));
+  }
+
+  /**
+   * @covers HashHelper::getChecksumAlgo
+   */
+  public function testGetChecksumAlgo() {
+    global $Controller;
+    $Controller = $this->createStub(ControllerInterface::class);
+    $stubConfig = $this->createStub(ConfigInterface::class);
+    $Controller->method('Config')->willReturn($stubConfig);
+    $stubConfig->method('__get')->willReturn('adler32');
+    $this->assertEquals('adler32', HashHelper::getChecksumAlgo());
   }
 
   /**
    * @covers HashHelper::getHashAlgo
    */
   public function testGetHashAlgo() {
-    $this->markTestSkipped(
-      'Waiting for Config redesign'
-    );
+    global $Controller;
+    $Controller = $this->createStub(ControllerInterface::class);
+    $stubConfig = $this->createStub(ConfigInterface::class);
+    $Controller->method('Config')->willReturn($stubConfig);
+    $stubConfig->method('__get')->willReturn('crc32b');
+    $this->assertEquals('crc32b', HashHelper::getHashAlgo());
   }
 
   /**
@@ -48,9 +59,42 @@ class HashHelperTest extends TestCase
    * @depends testGetHashAlgo
    */
   public function testHash() {
-    $this->markTestSkipped(
-      'Waiting for Config redesign'
-    );
+    $this->assertEquals('8c736521', HashHelper::hash('foo', 'crc32b'));
+    $this->assertEquals('76ff8caa', HashHelper::hash('bar', 'crc32b'));
+  }
+
+  /**
+   * @covers HashHelper::hash
+   * @depends testHash
+   * @dataProvider hashDataProvider
+   */
+  public function testHashwithParams(string $input, string $algo, string $expected) {
+    global $Controller;
+    $Controller = $this->createStub(ControllerInterface::class);
+    $stubConfig = $this->createStub(ConfigInterface::class);
+    $Controller->method('Config')->willReturn($stubConfig);
+    $stubConfig->method('__get')->willReturn($algo);
+    $this->assertEquals($expected, HashHelper::hash($input));
+    $this->assertEquals($expected, HashHelper::hash($input, $algo));
+  }
+
+  public function tokenDataProvider() {
+    return [
+      [0, 64],
+      [8, 64],
+      [9, 18],
+      [16, 32],
+      [32, 64],
+      [256, 512],
+    ];
+  }
+
+  public function hashDataProvider() {
+    return [
+      ['foo', 'adler32', '02820145'],
+      ['foo', 'crc32', 'a5c4fe49'],
+      ['foo', 'md5', 'acbd18db4cc2f85cedef654fccc4a4d8'],
+    ];
   }
 
 }
