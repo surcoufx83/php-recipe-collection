6b7886eebe7b8b7b807e2bcb2a228a6b9d3c9c3c
Stefan Fuchs <stefan@fuchs.pub>
2020-09-15 09:13:05 +0200

    Correct initialization of properties

--- private/entities/User/BlankUser.php
+++ private/entities/User/BlankUser.php
@@ -14,24 +14,23 @@ class BlankUser extends User {
   public function __construct(string $firstname, string $lastname, string $username, string $email) {
     $this->firstname = $firstname;
     $this->lastname = $lastname;
-    $this->name = $firstname.' '.$lastname;
     $this->mailadress = $email;
-    $this->username = $username;
+    $this->mailvalidationcode = HashHelper::generate_token(12);
+    $this->name = $firstname.' '.$lastname;
+    $this->passwordhash = '********';
     $this->registrationCompleted = new DateTime();
+    $this->username = $username;
   }
 
   public function sendActivationMail(array &$response) : bool {
     global $Controller, $twig, $OUT;
     $mail = new Mail();
-    $this->mailvalidationcode = HashHelper::generate_token(12);
     $OUT['ActivationLink'] = $Controller->getLink('private:activation', $this->mailvalidationcode);
     $data = [
       'Headline' => $Controller->l('sendmail_registration_activationMail_title'),
       'Content' => $twig->render('mails/activation-mail.html.twig', $OUT),
     ];
     if ($mail->send($this->name, $this->mailadress, $Controller->l('sendmail_registration_activationMail_subject'),  $data, $response)) {
-      $this->changes['user_email_validation'] = $this->mailvalidationcode;
-      $Controller->updateDbObject($this);
       return true;
     }
     return false;
@@ -40,8 +39,8 @@ class BlankUser extends User {
   public function save(array &$response) : bool {
     global $Controller;
     $result = $Controller->insertSimple('users',
-      ['user_name', 'user_firstname', 'user_lastname', 'user_fullname', 'user_password', 'user_email', 'user_registration_completed'],
-      [$this->username, $this->firstname, $this->lastname, $this->name, '********', $this->mailadress, $this->registrationCompleted->format(DTF_SQL)]
+      ['user_name', 'user_firstname', 'user_lastname', 'user_fullname', 'user_password', 'user_email', 'user_email_validation', 'user_registration_completed'],
+      [$this->username, $this->firstname, $this->lastname, $this->name, $this->passwordhash, $this->mailadress, $this->mailvalidationcode, $this->registrationCompleted->format(DTF_SQL)]
     );
     if ($result > -1) {
       $this->id = $result;
